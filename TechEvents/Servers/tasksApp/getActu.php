09ef16f846b22d8ab463c4f252756b998b6251d2
bousninayounes <49817278+bousninayounes@users.noreply.github.com>
2019-06-09 17:08:51 +0200

    actu

---
+++ TechEvents/Servers/tasksApp/getActu.php
@@ -0,0 +1,22 @@
+<?php header('Access-Control-Allow-Origin: *'); ?>
+<?php 
+require_once('connect.php'); 
+      $query_search = "SELECT * FROM `actualite`";  
+      $jsontext = "[";
+      $stmt1 = $pdo->query($query_search, PDO::FETCH_OBJ);
+			foreach($stmt1->fetchAll() as $row) {
+				$jsontext .="{";
+				foreach($row as $key => $value) {
+					if ($key!="photoEvent") {
+	    				$jsontext .= '"'.addslashes($key).'": "'.addslashes($value).'",';
+					}
+
+					}
+					$jsontext = substr_replace($jsontext, '', -1);
+					$jsontext .="},";
+	}
+	$jsontext = substr_replace($jsontext, '', -1);
+	$jsontext .= "]";
+	echo $jsontext;
+
+?>
